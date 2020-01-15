4f7569ade2fc711015f820a0a486ba2dd320b4c5
Otis Wolf (*) <owolf@gr-yx0cyjg5lmbp.local>
2020-01-15 11:37:41 -0600

    added delete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -135,4 +135,24 @@ router.get("/user/:user_id", async (req, res) => {
     }
 })
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user, and posts
+// @access  Public
+router.delete("/", auth,  async (req, res) => {
+    try {
+        // TODO: remove users posts
+
+        //remove profile
+        await Profile.findOneAndRemove( {user: req.user.id});
+
+        // remove user
+        await User.findOneAndRemove( {user: req.user.id })
+
+        res.json({ msg: "User deleted"});
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
 module.exports = router;
